4be571a41009aa389460f3a59190ff2fb87d1937
taha943 <tahafilali980@gmail.com>
2026-06-21 15:20:51 +0200

    first commit

---
+++ src/main/java/com/kata/Bank/account/kata/BankAccountKataApplication.java
@@ -0,0 +1,13 @@
+package com.kata.Bank.account.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/Bank/account/kata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.Bank.account.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
